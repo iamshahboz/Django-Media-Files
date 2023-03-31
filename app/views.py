168e63ee09792a82856b593cc271f6ee81d4e94e
unknown <sharafzodashahboz@gmail.com>
2023-03-31 18:00:59 +0600

    generics view class changed to APIView class

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
-from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
 from . serializers import DogSerializer
 from . models import Dog
 
@@ -9,13 +11,50 @@ from . models import Dog
 def homepage(request):
     return HttpResponse("This is your application")
 
-class DogListCreate(generics.ListCreateAPIView):
-    queryset = Dog.objects.all()
-    serializer_class = DogSerializer
+class DogListCreate(APIView):
+    #permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-class DogAction(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Dog.objects.all()
-    serializer_class = DogSerializer
+    def get(self,request):
+        dogs = Dog.objects.all()
+        serializer = DogSerializer(dogs,many=True)
+        return Response(serializer.data)
+    
+    def post(self,request):
+        serializer = DogSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+class DogDetail(APIView):
+    #permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get(self,request,pk):
+        dog = get_object_or_404(Dog,pk=pk)
+        serializer = DogSerializer(dog)
+        return Response(serializer.data)
+    
+    def put(self,request,pk):
+        dog = get_object_or_404(Dog,pk=pk)
+        serializer = DogSerializer(dog,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    def patch(self,request,pk):
+        dog = get_object_or_404(Dog,pk=pk)
+        serializer = DogSerializer(dog,data=request.data,partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self,request,pk):
+        dog = get_object_or_404(Dog,pk=pk)
+        dog.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
 
     
 
