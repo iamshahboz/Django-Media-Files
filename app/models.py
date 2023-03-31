6990f88e995e2d158ff32966631c985ba1d70e8e
unknown <sharafzodashahboz@gmail.com>
2023-03-31 17:59:34 +0600

    validator added for name field

--- app/models.py
+++ app/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from .validators import validate_string
 
 # Create your models here.
 
 class Dog(models.Model):
-    name = models.CharField(max_length=69)
+    name = models.CharField(max_length=69, validators=[validate_string])
     image = models.ImageField(upload_to='dogs/')
 
 # this method will allow you to delete the file from media root, while delete method called
