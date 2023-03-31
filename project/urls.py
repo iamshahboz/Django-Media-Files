26de08893ccf487bf94fa30fdaeee949dded481c
unknown <sharafzodashahboz@gmail.com>
2023-03-31 17:58:32 +0600

    view class name renamed

--- project/urls.py
+++ project/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from app.views import DogListCreate,DogAction
+from app.views import DogListCreate,DogDetail
 
 
 urlpatterns = [
@@ -10,7 +10,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('api/Dog',DogListCreate.as_view()),
-    path('api/Dog/<int:pk>',DogAction.as_view()),
+    path('api/Dog/<int:pk>',DogDetail.as_view()),
 
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
