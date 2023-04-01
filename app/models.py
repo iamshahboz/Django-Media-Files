1accc0b6734beeed553c714afd7da65dfd569c0f
unknown <sharafzodashahboz@gmail.com>
2023-04-02 03:00:40 +0600

    other fileupload restriction added

--- app/models.py
+++ app/models.py
@@ -1,11 +1,13 @@
 from django.db import models
-from .validators import validate_string
+from .validators import validate_string,validate_file_mimetype,ext_validator
+
+
 
 # Create your models here.
 
 class Dog(models.Model):
     name = models.CharField(max_length=69, validators=[validate_string])
-    image = models.ImageField(upload_to='dogs/')
+    image = models.FileField(upload_to='dogs/',validators=[ext_validator,validate_file_mimetype])
 
 # this method will allow you to delete the file from media root, while delete method called
     def delete(self):
