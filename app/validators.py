08db36f7d7866181c77525d2201a0997c93a7222
unknown <sharafzodashahboz@gmail.com>
2023-04-02 03:01:39 +0600

    function added to check the content

--- app/validators.py
+++ app/validators.py
@@ -1,4 +1,6 @@
 from django.core.exceptions import ValidationError
+from django.core.validators import FileExtensionValidator
+import magic 
 
 #this is for Dog model
 def validate_string(value:str):
@@ -7,3 +9,12 @@ def validate_string(value:str):
         if value.isdigit():
               raise ValidationError("Name must not be integer")
         return value
+
+ext_validator = FileExtensionValidator(['png','jpg','pdf'])
+
+def validate_file_mimetype(file):
+    accept = ['image/png','image/jpeg','application/pdf']
+    file_mime_type = magic.from_buffer(file.read(1024),mime=True)
+    if file_mime_type not in accept:
+        raise ValidationError('unsupported file type')
+    
