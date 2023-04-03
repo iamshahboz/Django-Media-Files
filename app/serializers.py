6e59d41b3cb8665954963ff73e307359acb4133f
unknown <sharafzodashahboz@gmail.com>
2023-04-03 14:28:19 +0600

    serializers for new models added

--- app/serializers.py
+++ app/serializers.py
@@ -1,10 +1,20 @@
 from rest_framework import serializers
-from . models import Dog
+from . models import Dog,Book,Author
 
 class DogSerializer(serializers.ModelSerializer):
     class Meta:
         model = Dog
         fields = ['id','name','image']
 
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book 
+        fields = ['id','name','price','number_in_stock','genre','author']
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ['id','name']
+        
     
         
