82dc0c577b175a9d8075208eb401777ed5050bc3
unknown <sharafzodashahboz@gmail.com>
2023-04-03 14:29:17 +0600

    url moved from project's to app's

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,20 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('',views.homepage, name='homepage'),
+
+    path('api/Dog',views.DogListCreate.as_view()),
+    path('api/Dog/<int:pk>',views.DogDetail.as_view()),
+
+    path('api/Book',views.BookListCreate.as_view()),
+    path('api/Book/<int:pk>',views.BookDetail.as_view()),
+
+    path('api/Author',views.AuthorListCreate.as_view()),
+    path('api/Author/<int:pk>',views.AuthorDetail.as_view()),
+
     
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
