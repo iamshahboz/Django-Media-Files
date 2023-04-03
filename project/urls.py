673cf068bdae54fd18883bf383917e109196eaad
unknown <sharafzodashahboz@gmail.com>
2023-04-03 14:31:31 +0600

    url moved from project's to app's

--- project/urls.py
+++ project/urls.py
@@ -1,16 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-from app.views import DogListCreate,DogDetail
+
+
 
 
 urlpatterns = [
     path('',include('app.urls')),
     path('admin/', admin.site.urls),
 
-    path('api/Dog',DogListCreate.as_view()),
-    path('api/Dog/<int:pk>',DogDetail.as_view()),
+   
 
 
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
